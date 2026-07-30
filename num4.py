511af1be0d04591bf6b3cd2f1db6b16fa4264c7c
Md.Muntasir Raiyan <mdmuntasirraiyan@gmail.com>
2026-07-31 01:14:35 +0600

    Add files via upload

---
+++ num4.py
@@ -0,0 +1,11 @@
+import numpy as np
+arr=np.random.randint(1,100,(20,20))
+print(arr)
+
+arr1=np.arange(1,10,1)
+arr2=np.arange(1,10,2)
+arr3=np.arange(1,10,1).reshape(3,3)
+print(arr1)
+print(arr2)
+print(arr3)
+
