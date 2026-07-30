3f095eed842fb5e52b8b26029a6b76390750db19
Md.Muntasir Raiyan <mdmuntasirraiyan@gmail.com>
2026-07-31 01:13:09 +0600

    Add files via upload

---
+++ num2.py
@@ -0,0 +1,15 @@
+import numpy as np
+lst=[1,2,3,4,5]
+arr=np.array(lst)
+print(arr)
+
+set={1,2,3,45,50}
+arr1=np.array((set))
+print(arr1)
+
+dic={"a":1,"b":2,"c":3}
+items=dic.items()
+arr=np.array(list(items))
+print(arr)
+
+
