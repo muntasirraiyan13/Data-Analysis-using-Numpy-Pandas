5f9c0c1fb3a9a7207c11c7d114ae84ffd063ea98
Md.Muntasir Raiyan <mdmuntasirraiyan@gmail.com>
2026-07-31 01:12:03 +0600

    Add files via upload

---
+++ num.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+arr1=np.array([1,2,3,4,5])
+arr2=np.array([[1,2,3],[4,5,6]])
+arr3=np.array([[1,2,5],[4,8,9],[1,5,2]])
+
+print(arr1.ndim)
+print(arr2.ndim)
+print(arr3.ndim)
+
+
+print(arr1.shape)
+print(arr2.shape)
+print(arr3.shape)
+
+
+print(arr1.size)
+print(arr2.size)
+print(arr3.size)
+
+print(arr1.dtype)
+print(arr2.dtype)
+print(arr3.dtype)
+
+
+
