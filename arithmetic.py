b177aa14a34f8018e078c668a6912584932eda1f
Md.Muntasir Raiyan <mdmuntasirraiyan@gmail.com>
2026-08-02 02:46:40 +0600

    Add files via upload

---
+++ arithmetic.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+x=np.array([10,20,30,40])
+y=np.array([2,3,4,5])
+add=x+y
+sub=x-y 
+multi=x*y 
+div=x/y 
+print(add)
+print(sub)
+print(multi)
+print(div)
+
+sin_value=np.sin(x)
+print(sin_value)
+cos_value=np.cos(x)
+print(cos_value)
+
+greater_than=x>y 
+print(greater_than)
+equal= x==y
+print(equal)
+
+
+
